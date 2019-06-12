4120dc00230fe2d32933fc5945ef6c3ff7b4a462
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-06-12 19:05:07 +0200

    added button

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 const quotes = 'https://api.myjson.com/bins/u1359';
 const quotediv = document.getElementById("quote");
-fetch(quotes)
-  .then((resp) => resp.json())
-  .then((data) => displayQuote(data));
+const nextquote = document.getElementById("nextquote");
 
-function displayQuote(data){
-    console.log(data);
+document.addEventListener("click" , function updatePage() {
+  fetch(quotes)
+    .then((resp) => resp.json())
+    .then((data) => displayQuote(data));
 
-    quotediv.innerHTML = `<p>${data[Math.floor(Math.random() * 40)].quote}</p>`
-  };
+  function displayQuote(data){
+      console.log(data);
+      let randomNumber = Math.floor(Math.random() * 40);
+      quotediv.innerHTML = `<p>${data[randomNumber].quote}</p>`
+
+      setTimeout(function () {
+
+        let string = quotediv.innerHTML;
+
+        if (string.indexOf("Fry") > -1) {
+          string = string.replace(/Fry/g, '<span style="color:orange;">Fry</span>');
+        }
+
+        if (string.indexOf("Leela") > -1) {
+          string = string.replace(/Leela/g, '<span style="color:purple;">Leela</span>');
+        }
+
+        if (string.indexOf("Zoidberg") > -1) {
+          string = string.replace(/Zoidberg/g, '<span style="color:red;">Zoidberg</span>');
+        }
+
+        if (string.indexOf("Bender") > -1) {
+          string = string.replace(/Bender/g, '<span style="color:grey;">Bender</span>');
+        }
+
+        quotediv.innerHTML = string;
+
+      }, 10);
+
+    };
+});
